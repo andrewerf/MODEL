30ea81118d095714610a12e4c644eed28841bcdb
Andrew <andrew-aralov@yandex.ru>
2024-09-20 22:42:33 +0200

    Remove unneeded computations.

--- include/LU.hpp
+++ include/LU.hpp
@@ -3,6 +3,18 @@
 //
 #pragma once
 
+template <typename T, Index n, Index m>
+void print( const Mat<T, n, m>& mat )
+{
+    for ( Index i = 0; i < n; ++i )
+    {
+        for ( Index j = 0; j < m; ++j )
+        {
+            std::cout << mat( i, j ) << ' ';
+        }
+        std::cout << '\n';
+    }
+}
 
 
 template <typename T, Index n>
@@ -22,30 +34,17 @@ LU_Result<T, n> LU( Mat<T, n, n> mat )
     {
         Mat<T, n, n> L_inv_0;
         for ( Index j = 0; j < n; ++j )
-            L_inv_0( j, j ) = 1.f;
+            L( j, j ) = 1.f;
 
         for ( Index j = k + 1; j < n; ++j )
         {
             const auto alpha = -mat( j, k ) / mat( k, k );
-            L_inv_0( j, j ) = 1;
-            L_inv_0( j, k ) = alpha;
+            L( j, j ) = 1;
+            L( j, k ) = -alpha;
 
             for ( Index i = 0; i < n; ++i )
                 mat( j, i ) = mat( j, i ) + alpha * mat( k, i );
         }
-
-        Mat<T, n, n> L_0;
-        for ( Index i = 0; i < n; ++i )
-            L_0( i, i ) = T( 1 );
-        for ( Index i = 1; i < n; ++i )
-        {
-            for ( Index j = 0; j < i; ++j )
-            {
-                L_0( i, j ) = -L_inv_0( i, j );
-            }
-        }
-
-        L = L * L_0;
     }
 
     return { L, mat };
