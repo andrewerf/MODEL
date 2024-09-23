753e808a5e3d491737ac884ed903c6708ebf11b2
Andrew <andrew-aralov@yandex.ru>
2024-09-23 09:29:36 +0200

    Add examples folder and `pluq_simple` example.

---
+++ examples/pluq_simple.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <array>
+#include <cassert>
+#include "Mat.hpp"
+#include "LU.hpp"
+
+
+int main()
+{
+    Mat<float, 4, 4> mat;
+    mat( 0, 0 ) = 1;
+    mat( 0, 1 ) = 2;
+    mat( 0, 2 ) = 3;
+    mat( 0, 3 ) = 4;
+
+    mat( 1, 0 ) = 1;
+    mat( 1, 1 ) = 1;
+    mat( 1, 2 ) = 1;
+    mat( 1, 3 ) = 1;
+
+    mat( 2, 0 ) = 4;
+    mat( 2, 1 ) = 2;
+    mat( 2, 2 ) = 3;
+    mat( 2, 3 ) = 1;
+
+    mat( 3, 0 ) = 2;
+    mat( 3, 1 ) = 3;
+    mat( 3, 2 ) = 1;
+    mat( 3, 3 ) = 1;
+
+    auto [P, L, U, Q] = PLUQ( mat );
+    print( P );
+    std::cout << '\n';
+    print( L );
+    std::cout << '\n';
+    print( U );
+    std::cout << '\n';
+    print( Q );
+    std::cout << '\n';
+
+    auto check = P * L * U * Q;
+    print( check );
+    std::cout << '\n';
+
+    return 0;
+}
