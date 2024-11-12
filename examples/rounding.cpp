4ca3842e90981bcb350698aef5d427d21c5e204f
Andrew <andrew-aralov@yandex.ru>
2024-11-12 22:04:24 +0100

    Remove unfinished work.

--- examples/rounding.cpp
+++ examples/rounding.cpp
@@ -8,7 +8,7 @@
 int main()
 {
     double a = 1;
-    double b = pow( double( 2 ), -53 );
+    double b = pow( double( 2 ), 90 );
     double c = a + b;
 
     std::cout << std::setprecision( 10000 ) << a << '\n' << b << '\n' << c << '\n';
