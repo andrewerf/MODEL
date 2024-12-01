81bee3ef7d8b13634d00b238f1a7c933046b792d
Andrew <andrew-aralov@yandex.ru>
2024-12-01 20:23:05 +0100

    Move benchmark to benchmarks.

--- benchmark/benchmark_strassen.cpp
+++ benchmark/benchmark_strassen.cpp
@@ -1,7 +1,5 @@
-//
-// Created by Andrey Aralov on 11/25/24.
-//
 #include <StrassenInversion.hpp>
+#include <StrassenMultiplication.hpp>
 #include <random.hpp>
 #include <benchmark/benchmark.h>
 
@@ -11,7 +9,7 @@ using namespace M;
 constexpr auto cTimeUnit = benchmark::kMillisecond;
 
 
-static void BM_NaiveStrassenInversionBenchmark( benchmark::State& state )
+static void BM_NaiveStrassenInversion( benchmark::State& state )
 {
     auto sz = state.range( 0 );
     auto mat = generateRandomMatrix<float>( sz, sz );
@@ -21,4 +19,47 @@ static void BM_NaiveStrassenInversionBenchmark( benchmark::State& state )
     }
 }
 
-BENCHMARK(BM_NaiveStrassenInversionBenchmark)->DenseRange( 100, 2000, 200 )->Unit( cTimeUnit );
+BENCHMARK(BM_NaiveStrassenInversion)->DenseRange( 100, 2000, 200 )->Unit( cTimeUnit );
+
+static void BM_StrassenMultiplication( benchmark::State& state )
+{
+    auto m = state.range( 0 );
+    auto n = state.range( 1 );
+    auto p = state.range( 2 );
+    auto a = generateRandomMatrix(m, n);
+    auto b = generateRandomMatrix(n, p);
+    for ( auto _ : state )
+    {
+        benchmark::DoNotOptimize( multiplyStrassen( a, b ) );
+    }
+}
+
+BENCHMARK(BM_StrassenMultiplication)
+    ->Args(
+        // 1.2s [BLAS: 68ms]
+        { 2222, 2222, 2222 }
+    )
+    ->Args(
+        // 3.9s [BLAS: 184ms]
+        { 2222, 3333, 4444 }
+    )
+// The following test cases were used during development to validate the
+// splitting of wide or tall matrices into squarish blocks
+// in multiplySubmatrix and to set the constants kInnerSplitRatio
+// and kOuterSplitRatio
+    ->Args(
+        // 1.1s (1.9s without split) [BLAS: 45ms]
+        { 500, 4000, 4000 }
+    )
+    ->Args(
+        // 1.1s (1.9s without split) [BLAS: 44ms]
+        { 4000, 4000, 500 }
+    )
+    ->Args(
+        // 1.3s [BLAS: 71ms]
+        { 4000, 800, 4000 }
+    )
+    ->Args(
+        // 0.8s (1.3s without split) [BLAS: 27ms]
+        { 800, 8000, 800 }
+    )->Unit( cTimeUnit );
--- test/test_strassen_multiplication.cpp
+++ test/test_strassen_multiplication.cpp
@@ -34,13 +34,6 @@ TEST_P(TestStrassenMultiplication, CompareWithNaiveMultiplication) {
     EXPECT_TRUE(isMatrixNear(c, a * b));
 }
 
-TEST_P(BenchmarkStrassenMultiplication, Benchmark) {
-    auto [m, n, p] = GetParam();
-    auto a = generateRandomMatrix(m, n);
-    auto b = generateRandomMatrix(n, p);
-    auto c = multiplyStrassen(a, b);
-}
-
 const MatrixProductSize kTestSizes[] = {
     { 3, 4, 5 },
     { 30, 40, 50 },
@@ -52,37 +45,10 @@ const MatrixProductSize kTestSizes[] = {
     { 129, 222, 128 },
 };
 
-const MatrixProductSize kBenchmarkSizes[] = {
-    // 1.2s [BLAS: 68ms]
-    { 2222, 2222, 2222 },
-    // 3.9s [BLAS: 184ms]
-    { 2222, 3333, 4444 },
-
-    // The following test cases were used during development to validate the
-    // splitting of wide or tall matrices into squarish blocks
-    // in multiplySubmatrix and to set the constants kInnerSplitRatio
-    // and kOuterSplitRatio
-
-    // 1.1s (1.9s without split) [BLAS: 45ms]
-    { 500, 4000, 4000 }, 
-    // 1.1s (1.9s without split) [BLAS: 44ms]
-    { 4000, 4000, 500 },
-    // 1.3s [BLAS: 71ms]
-    { 4000, 800, 4000 },
-    // 0.8s (1.3s without split) [BLAS: 27ms]
-    { 800, 8000, 800 },
-};
-
 INSTANTIATE_TEST_SUITE_P(
     TestStrassenMultiplicationAllSizes,
     TestStrassenMultiplication,
     testing::ValuesIn(kTestSizes),
-    ProductSizeToString());
-
-INSTANTIATE_TEST_SUITE_P(
-    BenchmarkStrassenMultiplicationAllSizes,
-    BenchmarkStrassenMultiplication,
-    testing::ValuesIn(kBenchmarkSizes),
     ProductSizeToString());
 
 }  // namespace
