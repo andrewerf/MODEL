012436163a446992fce29d174fbbd944f499adcb
burlemarxiste <emilie.o.gillet+github@gmail.com>
2024-12-09 01:26:03 +0100

    Finer grained benchmarks

--- benchmark/benchmark_strassen.cpp
+++ benchmark/benchmark_strassen.cpp
@@ -20,6 +20,19 @@ using namespace M;
 constexpr auto cTimeUnit = benchmark::kMillisecond;
 using T = double;
 
+static void BM_NaiveMultiplication( benchmark::State& state )
+{
+    auto n = state.range( 0 );
+    auto a = generateRandomMatrix<T>(n, n);
+    auto b = generateRandomMatrix<T>(n, n);
+    for ( auto _ : state )
+    {
+        benchmark::DoNotOptimize( a * b );
+    }
+}
+
+BENCHMARK(BM_NaiveMultiplication)->DenseRange( 100, 2000, 200 )->Unit( cTimeUnit );
+
 // Returns a version of multiplyStrassen specialized for a given value of
 // min_size (minimum size of a leaf product)
 template <Index min_size>
@@ -75,21 +88,21 @@ static void invBlockSizeAndInputSizeArgs( benchmark::internal::Benchmark* b )
 static void BM_NaiveStrassenInversion( benchmark::State& state )
 {
     auto sz = state.range( 0 );
-    auto mat = generateRandomMatrix<float>( sz, sz );
+    auto mat = generateRandomMatrix<T>( sz, sz );
     for ( auto _ : state )
     {
         benchmark::DoNotOptimize( inverseStrassen( mat ) );
     }
 }
 
-BENCHMARK(BM_NaiveStrassenInversion)->DenseRange( 100, 2000, 500 )->Unit( cTimeUnit );
+BENCHMARK(BM_NaiveStrassenInversion)->DenseRange( 100, 2000, 200 )->Unit( cTimeUnit );
 
 
 static void BM_StrassenInversion( benchmark::State& state )
 {
     auto inv_bs = state.range( 0 );
     auto sz = state.range( 2 );
-    auto mat = generateRandomMatrix<float>( sz, sz );
+    auto mat = generateRandomMatrix<T>( sz, sz );
     auto mult = getStrassenMult( state.range( 1 ) );
     for ( auto _ : state )
     {
@@ -97,13 +110,13 @@ static void BM_StrassenInversion( benchmark::State& state )
     }
 }
 
-BENCHMARK(BM_StrassenInversion)->Apply( invBlockSizeAndInputSizeArgs<100, 2600, 500> )->Unit( cTimeUnit );
+BENCHMARK(BM_StrassenInversion)->Apply( invBlockSizeAndInputSizeArgs<100, 2600, 200> )->Unit( cTimeUnit );
 
 static void BM_StrassenMultiplication( benchmark::State& state )
 {
     auto n = state.range( 1 );
-    auto a = generateRandomMatrix<float>(n, n);
-    auto b = generateRandomMatrix<float>(n, n);
+    auto a = generateRandomMatrix<T>(n, n);
+    auto b = generateRandomMatrix<T>(n, n);
     auto mult = getStrassenMult( state.range( 0 ) );
     for ( auto _ : state )
     {
@@ -111,4 +124,31 @@ static void BM_StrassenMultiplication( benchmark::State& state )
     }
 }
 
-BENCHMARK(BM_StrassenMultiplication)->Apply( multBlockSizeAndInputSizeArgs<100, 2600, 500> )->Unit( cTimeUnit );
+BENCHMARK(BM_StrassenMultiplication)->Apply( multBlockSizeAndInputSizeArgs<100, 3300, 200> )->Unit( cTimeUnit );
+BENCHMARK(BM_StrassenMultiplication)->Apply( multBlockSizeAndInputSizeArgs<2000, 8000, 2000> )->Unit( cTimeUnit );
+
+static void BM_TiledMultiplication( benchmark::State& state )
+{
+    auto n = state.range( 0 );
+    auto a = generateRandomMatrix<T>(n, n);
+    auto b = generateRandomMatrix<T>(n, n);
+    for ( auto _ : state )
+    {
+        benchmark::DoNotOptimize( multiplyTiled(a, b) );
+    }
+}
+
+BENCHMARK(BM_TiledMultiplication)->DenseRange( 100, 3300, 200 )->Unit( cTimeUnit );
+BENCHMARK(BM_TiledMultiplication)->DenseRange( 2000, 8000, 2000 )->Unit( cTimeUnit );
+
+/*static void BM_StrassenInversionFast( benchmark::State& state )
+{
+    auto sz = state.range( 0 );
+    auto mat = generateRandomMatrix<T>( sz, sz );
+    for ( auto _ : state )
+    {
+        benchmark::DoNotOptimize( inverseStrassenFast( mat ) );
+    }
+}
+
+BENCHMARK(BM_StrassenInversionFast)->DenseRange( 100, 3300, 200 )->Unit( cTimeUnit );*/
